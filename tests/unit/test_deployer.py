778dfdaf220ab14873cbb5c85ce3e48d2346aee1
Srikant Shubham <srikantshubham.personal@gmail.com>
2026-04-17 19:28:04 +0530

    [WO-1] Delete drifted modules

--- tests/unit/test_deployer.py
+++
@@ -1,9 +0,0 @@
-from evidentia.deployer import can_deploy
-
-
-def test_deploy_requires_approval():
-    assert can_deploy({"approved": False}) is False
-
-
-def test_deploy_requires_boolean_approval():
-    assert can_deploy({"approved": "false"}) is False
--- tests/unit/test_review_gate.py
+++
@@ -1,9 +0,0 @@
-from evidentia.cli import approve_spec
-
-
-def test_build_is_blocked_without_approval():
-    assert approve_spec({"approved": False}) is False
-
-
-def test_build_is_blocked_for_string_approval_values():
-    assert approve_spec({"approved": "false"}) is False
